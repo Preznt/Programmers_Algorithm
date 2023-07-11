a528b4563fdce762243d8939f98959466832761f
Preznt <bjw1403@gmail.com>
2023-07-11 18:45:44 +0900

    오류난 부분 정리

--- AdditionFractions.java
+++ AdditionFractions.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 
 /**
  * 7/11 분수의 덧셈
+ * 첫 번째 분수의 분자와 분모를 뜻하는 numer1, denom1, 두 번째 분수의 분자와 분모를 뜻하는 numer2, denom2가 매개변수로 주어집니다.
+ * 두 분수를 더한 값을 기약 분수로 나타냈을 때 분자와 분모를 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
  */
 public class AdditionFractions {
     // 소인수분해는 어떤 수를 소수인 인수로 분해하는 것
@@ -9,8 +11,9 @@ public class AdditionFractions {
         public int[] solution(int numer1, int denom1, int numer2, int denom2) {
             int numerator = numer1 * denom2 + numer2 * denom1;
             int denominator = denom1 * denom2;
-            int[] answer = new int[2];
 
+            // for 반복문 조건을 작은수에서 큰순서로 하게 되면
+            // 나눴을 때 끝부분에서 기약분수가 되지 않았는데 큰 수만 남아있어 조건이 성립되지 않아 오류가 발생하여 아래와 같이 수정
             for(int i = numerator  ; i > 1 ; i--){
                 if(denominator % i == 0 && numerator % i == 0){
                    numerator /= i;
@@ -18,10 +21,7 @@ public class AdditionFractions {
                 }
 
             }
-
-            answer[0] = numerator;
-            answer[1] = denominator;
-
+            int[] answer = {numerator, denominator};
             return answer;
         }
 
