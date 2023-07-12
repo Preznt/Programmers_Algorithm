aab2802ac82b88476e90583d67f5635a98327b93
Preznt <bjw1403@gmail.com>
2023-07-12 18:32:09 +0900

    자바 스트림 정리

--- DoubleArray.java
+++ DoubleArray.java
@@ -7,6 +7,9 @@ import java.util.Arrays;
 public class DoubleArray {
     public int[] solution(int[] numbers) {
         return Arrays.stream(numbers).map(number -> number * 2).toArray();
-
+        // 자바 스트림
+        // Java 8에서 추가된 기능으로 컬렉션에 저장되어있는 엘리먼트들을 하나씩 순회하면서 처리할 수 있는 코드패턴
+        // 람다식과 함께 표현되어 간결한 표현 가능
+        // 내부 반복자와 함꼐 사용하기 때문에 병렬처리가 쉽다
     }
 }
