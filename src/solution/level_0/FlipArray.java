20d576739b90b37a351fa966d5f61427299bf9a2
Preznt <bjw1403@gmail.com>
2023-07-19 18:06:22 +0900

    좀 더 보기편한 식으로 변경

--- src/solution/level_0/FlipArray.java
+++ src/solution/level_0/FlipArray.java
@@ -9,9 +9,14 @@ public class FlipArray {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length];
 
+        /*
         for(int i = num_list.length ; i > 0 ; i--){
             answer[num_list.length - i] = num_list[i -1];
         };
+        */
+        for(int i = 0; i< num_list.length; i++){
+            answer[i] = num_list[num_list.length-i-1];
+        }
 
         return answer;
     }
