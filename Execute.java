42ccc2c810677a8904799cf5d64abd2aacabf9d6
Preznt <bjw1403@gmail.com>
2023-07-19 18:05:14 +0900

    배열 뒤집기

--- Execute.java
+++ Execute.java
@@ -1,11 +1,13 @@
 import solution.level_0.DividePizza3;
+import solution.level_0.FlipArray;
 
 /**
  * 실행을 위한 클래스
  */
 public class Execute {
     public static void main(String[] args) {
-     DividePizza3 dividePizza3 = new DividePizza3();
-     dividePizza3.wrongSolution(10, 5 );
+        FlipArray flipArray = new FlipArray();
+        int[] array = {4,6,2,4,1};
+        flipArray.solution(array);
     }
 }
---
+++ src/solution/level_0/FlipArray.java
@@ -0,0 +1,18 @@
+package solution.level_0;
+
+/**
+ * 배열 뒤집기
+ * 정수가 들어 있는 배열 num_list가 매개변수로 주어집니다.
+ * num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
+ */
+public class FlipArray {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+
+        for(int i = num_list.length ; i > 0 ; i--){
+            answer[num_list.length - i] = num_list[i -1];
+        };
+
+        return answer;
+    }
+}
