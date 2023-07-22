35c402da34da3dce37946884ec92a057951746a6
Preznt <bjw1403@gmail.com>
2023-07-22 16:50:42 +0900

    특정 문자 제거하기

--- Execute.java
+++ Execute.java
@@ -1,14 +1,11 @@
-import solution.level_0.DividePizza3;
-import solution.level_0.FlipArray;
-import solution.level_0.FlipString;
-import solution.level_0.RepeatString;
+import solution.level_0.*;
 
 /**
  * 실행을 위한 클래스
  */
 public class Execute {
     public static void main(String[] args) {
-        RepeatString repeatString = new RepeatString();
-        repeatString.solution("hello", 3);
+        DeleteString deleteString = new DeleteString();
+        deleteString.solution("abcdefe", "e");
     }
 }
